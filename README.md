55ac99bd114cb04278cbd05c6f938bc223e47962
Marcin Kitowicz <marcin.kitowicz@gmail.com>
2020-04-15 22:30:41 +0200

    My first action is ready

--- README.md
+++ README.md
@@ -1,1 +1,83 @@
 # dartanalyzer-annotations-action
+
+Creates [annotations](https://developer.github.com/v3/checks/runs/) from dartanalyzer output.
+This action requires installed `dartanalyzer` - you can provide it by adding another action for example [flutter-action](https://github.com/subosito/flutter-action).
+Recommended is to use with one of the lint rulesets: 
+- [effective_dart](https://pub.dev/packages/effective_dart)
+- [pedantic](https://pub.dev/packages/pedantic)
+
+
+## Inputs
+
+### `check_name`
+
+**Required** The name of the check run to add annotations to. This should be the same as the job that uses this action.
+
+### `commit_sha`
+
+**Required** Commit to attach the check to. If the trigger is `push`, this should just be `github.sha`. If the trigger is `pull_request`, this should be the `github.event.pull_request.head.sha`.
+
+
+## Example usage 
+
+On pull requests branch:
+
+```
+name: Run lint on pull requests
+on:
+  pull_request:
+    branches: [ develop ]
+
+jobs:
+  lint:
+    name: Check code style
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout source code
+        uses: actions/checkout@v2
+      - name: Install Flutter
+        uses: subosito/flutter-action@v1
+        with:
+          channel: 'stable'
+      - name: Get packages in a Flutter project
+        run: flutter pub get
+      - name: Run lint
+        uses: kitek/dartanalyzer-annotations-action@v1  
+        env:
+          GITHUB_TOKEN: ${{ github.token }}
+        with:
+          check_name: 'Check code style'
+          commit_sha: ${{ github.event.pull_request.head.sha }}
+
+```
+
+On develop branch:
+
+```
+name: Build and release app to beta testers
+
+on:
+  push:
+    branches: [ develop ]
+jobs:
+  build-android:
+    name: Build Android App
+    runs-on: ubuntu-latest
+    steps:
+      - name: Checkout Action
+        uses: actions/checkout@v2
+      - name: Install Flutter
+        uses: subosito/flutter-action@v1
+        with:
+          channel: 'stable'
+      - name: Get packages in a Flutter project
+        run: flutter pub get
+      - name: Run lint
+        uses: kitek/dartanalyzer-annotations-action@v1  
+        env:
+          GITHUB_TOKEN: ${{ github.token }}
+        with:
+          check_name: 'Build Android App'
+          commit_sha: ${{ github.sha }}
+
+```
---
+++ src/index.js
@@ -0,0 +1,85 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+const exec = require('@actions/exec');
+
+const { GITHUB_TOKEN, GITHUB_WORKSPACE } = process.env;
+
+async function main() {
+    try {
+        const output = await runAnalyzer();
+        const annotations = parseOutput(output);
+        await createCheck(annotations);
+    } catch (error) {
+      core.setFailed(error.message);
+    }
+}
+
+async function runAnalyzer() {
+    let output = '';
+    const options = {};
+    options.listeners = {
+        stderr: (data) => {
+            output += data.toString();
+        }
+    };
+
+    await exec.exec('dartanalyzer --format machine --options analysis_options.yaml ./lib', [], options);
+
+    return output.trim();
+}
+
+function parseOutput(output) {
+    let annotations = [];
+    if(output.length == 0) return annotations;
+
+    const lines = output.trim().split(/\r?\n/);
+    const cwd = `${process.cwd()}/`;
+
+    lines.forEach(line => {
+        const values = line.split('|');
+
+        const type = values[2];
+        const filePath = values[3].replace(cwd, '');
+        const lineNumber = parseInt(values[4], 10);
+        const colStart = parseInt(values[5], 10);
+        const colEnd = colStart + parseInt(values[6], 10);
+        const docLink = `https://dart-lang.github.io/linter/lints/${type}.html`;
+        const message = `${values[7]}\n${docLink}`;
+
+        const annotation = {
+            path: filePath,
+            start_line: lineNumber,
+            end_line: lineNumber,
+            start_column: colStart,
+            end_column: colEnd,
+            annotation_level: 'warning',
+            message: message,
+        };
+        annotations.push(annotation);
+    });
+
+    return annotations.slice(0, 49);
+}
+
+async function createCheck(annotations) {
+    if(annotations.length == 0) return;
+
+    const checkName = core.getInput('check_name');
+    const octokit = new github.GitHub(String(GITHUB_TOKEN));
+    const req = Object.assign({}, github.context.repo, { ref: core.getInput('commit_sha') });
+    const res = await octokit.checks.listForRef(req);
+    const check_run_id = res.data.check_runs.filter(check => check.name === checkName)[0].id;
+    const update_req = Object.assign({}, github.context.repo, {
+        check_run_id,
+        output: {
+            title: 'failures detected',
+            summary: `${annotations.length} errors(s) found`,
+            annotations
+        }
+    });
+    await octokit.checks.update(update_req);
+
+    core.setFailed(`${annotations.length} errors(s) found`);
+}
+
+main();
